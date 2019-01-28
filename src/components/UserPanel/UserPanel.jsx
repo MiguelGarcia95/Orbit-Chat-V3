1c35013cf9e6b60d3e8da5b11d9586252fb800e9
Miguel <mgarcia95951@gmail.com>
2019-01-27 19:46:49 -0600

    set up app template/format

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -3,11 +3,29 @@ import {Grid, Image, Container} from 'semantic-ui-react';
 
 const UserPanel = () => {
   return(
-    <Grid className='sidebar_user' >
+    <React.Fragment>
+      <Grid className='sidebar_user' >
       <Container fluid>
         <Image src='/img/ChatLogo.png' rounded centered />
       </Container>
     </Grid>
+    {/* Modal for Settings Modal */}
+        {/* <Modal size='small' basic centered={false} >
+          <Modal.Header>Settings</Modal.Header>
+          <Modal.Content>
+            <Segment>
+              <Label attached='top' >Name</Label>
+              <Input fluid placeholder='Category Name' name='name' />
+            </Segment>
+            <Button.Group attached='bottom'>
+              <Button negative >Cancel</Button>
+              <Button.Or />
+              <Button positive >Save</Button>
+            </Button.Group>
+          </Modal.Content>
+        </Modal> */}
+    </React.Fragment>
+    
   )
 }
 
