d787bb69c9cb49224a107c64545f3d435a10303b
Miguel <mgarcia95951@gmail.com>
2019-01-27 20:02:16 -0600

    set up app template/format

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import {Grid, Image, Container} from 'semantic-ui-react';
+import {Grid, Image, Container, Modal, Segment, Button, Label, Input} from 'semantic-ui-react';
 
-const UserPanel = () => {
-  return(
-    <React.Fragment>
-      <Grid className='sidebar_user' >
-      <Container fluid>
-        <Image src='/img/ChatLogo.png' rounded centered />
-      </Container>
-    </Grid>
-    {/* Modal for Settings Modal */}
-        {/* <Modal size='small' basic centered={false} >
-          <Modal.Header>Settings</Modal.Header>
-          <Modal.Content>
-            <Segment>
-              <Label attached='top' >Name</Label>
-              <Input fluid placeholder='Category Name' name='name' />
-            </Segment>
-            <Button.Group attached='bottom'>
-              <Button negative >Cancel</Button>
-              <Button.Or />
-              <Button positive >Save</Button>
-            </Button.Group>
-          </Modal.Content>
-        </Modal> */}
-    </React.Fragment>
-    
-  )
+class UserPanel extends React.Component {
+  state = {
+    modal: false
+  }
+
+  openModal = () => this.setState({modal: true});
+  closeModal = () => this.setState({modal: false});
+
+  render() {
+    const {modal} = this.state;
+    return(
+      <React.Fragment>
+        <Grid className='sidebar_user' >
+        <Container fluid>
+          {/* Avatar */}
+          <Image src='/img/ChatLogo.png' rounded centered onClick={this.openModal} />
+        </Container>
+      </Grid>
+      {/* Modal for Settings Modal */}
+      <Modal size='small' basic centered={false} open={modal} >
+        <Modal.Header>Settings</Modal.Header>
+        <Modal.Content>
+          <Segment>
+            <Label attached='top' >Name</Label>
+            <Input fluid placeholder='Category Name' name='name' />
+          </Segment>
+          <Button.Group attached='bottom'>
+            <Button negative onClick={this.closeModal} >Cancel</Button>
+            <Button.Or />
+            <Button positive >Save</Button>
+          </Button.Group>
+        </Modal.Content>
+      </Modal>
+      </React.Fragment>
+    )
+  }
 }
 
 export default UserPanel;
