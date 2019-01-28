4343a824cbddc435d69f630bf11f868172941c26
Miguel <mgarcia95951@gmail.com>
2019-01-28 14:23:06 -0600

    set up auth logic

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,11 +1,20 @@
 import * as actionTypes from './types';
 
 export const signIn = credentials => {
-  return (dispatch, getState, {getFirestore, getFirebase}) => {
+  return (dispatch, getState, {getFirebase}) => {
     const firebase = getFirebase();
-    const firestore = getFirestore();
-    console.log(credentials)
-
+    firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({
+          type: actionTypes.SIGN_IN,
+          payload: {authError: null}
+        })
+      }).catch(err => {
+        dispatch({
+          type: actionTypes.SIGN_IN,
+          payload: {authError: err.message}
+        })
+      })
   }
 }
 
@@ -14,34 +23,30 @@ export const signUp = credentials => {
     const firebase = getFirebase();
     const firestore = getFirestore();
     firebase.auth().createUserWithEmailAndPassword(credentials.email, user.password)
-    .then(createdUser => {
-      //update user
-      createdUser.user.updateProfile({
-        displayName: credentials.username,
-        photoURL: `http://gravatar.com/avatar/${md5(credentials.email)}?d=identicon`
-      })
+      .then(createdUser => {
+        //update user
+        createdUser.user.updateProfile({
+          displayName: credentials.username,
+          photoURL: `http://gravatar.com/avatar/${md5(credentials.email)}?d=identicon`
+        })
 
-      //Create Firestore user
-      firestore.add('users', {
-        username: credentials.username,
-        avatar: `http://gravatar.com/avatar/${md5(credentials.email)}?d=identicon`,
-        uid: createdUser.user.uid,
-        email: credentials.email
-      });
-    }).then(() => {
-      dispatch({
-        type: actionTypes.SIGN_UP,
-        payload: {
-          authError: null
-        }
+        //Create Firestore user
+        firestore.add('users', {
+          username: credentials.username,
+          avatar: `http://gravatar.com/avatar/${md5(credentials.email)}?d=identicon`,
+          uid: createdUser.user.uid,
+          email: credentials.email
+        });
+      }).then(() => {
+        dispatch({
+          type: actionTypes.SIGN_UP,
+          payload: {authError: null}
+        })
+      }).catch(err => {
+        dispatch({
+          type: actionTypes.SIGN_UP,
+          payload: {authError: err.message}
+        })
       })
-    }).catch(err => {
-      dispatch({
-        type: actionTypes.SIGN_UP,
-        payload: {
-          authError: err.message
-        }
-      })
-    })
   }
 }
