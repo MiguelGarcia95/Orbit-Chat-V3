ee305d0a71b0de2c0bde75ed2fb341edade03964
Miguel <mgarcia95951@gmail.com>
2019-01-28 16:48:31 -0600

    set up auth logic

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,14 +1,31 @@
 import * as actionTypes from '../actions/types';
 
 const initialState = {
-  currentUser: null
+  currentUser: null,
+  authError: null
 }
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.SIGN_UP:
       return {
-        ...state
+        ...state,
+        authError: action.payload.authError
+      }
+    case actionTypes.SIGN_IN:
+      return {
+        ...state,
+        authError: action.payload.authError
+      }
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        currentUser: action.payload.currentUser
+      }
+    case actionTypes.UNSET_USER:
+      return {
+        ...state,
+        currentUser: action.payload.currentUser
       }
     default:
       return state;
