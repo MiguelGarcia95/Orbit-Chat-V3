6163cdfd3f418d982062f243ba1a7d83394a6981
Miguel <mgarcia95951@gmail.com>
2019-01-28 22:57:36 -0600

    set up current user

--- src/actions/chatroomActions.js
+++ src/actions/chatroomActions.js
@@ -1,2 +1,13 @@
 import * as actionTypes from './types';
 
+export const createChatroom = chatroom => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(chatroom)
+  }
+}
+
+export const getChatroom = chatroomId => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(chatroomId)
+  }
+}
