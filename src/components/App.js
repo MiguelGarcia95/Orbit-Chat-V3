cfdc10180b4ded2700ec58e196f0a9b6306e7b86
Miguel <mgarcia95951@gmail.com>
2019-01-29 14:24:35 -0600

    remove individual menus

--- src/components/App.js
+++ src/components/App.js
@@ -22,46 +22,14 @@ class App extends React.Component {
     const {user} = this.props;
     return !user ? <Spinner /> : (
       <Grid columns='equal'>
-        <Sidebar 
+        {/* <Sidebar 
           width='very thin'
           icon='labeled'
           as={Menu}
           inverted
           vertical
           visible
-        />
-
-        {/* <Menu
-        size='large' 
-        fixed='left'
-        vertical
-        // style={{paddingTop: '30px'}}
-        className='Chatroom_Header'
-      >
-        <Menu.Header 
-          as='div' 
-          content={
-            <Grid>
-              <Grid.Row columns='2'>
-                <Grid.Column verticalAlign='middle' width={12}>
-                  <Container style={{width: '100%', overflow: 'hidden'}} >
-                    {'Chatroom Name'}
-                  </Container>
-                </Grid.Column>
-                <Grid.Column verticalAlign='middle' width={2}>
-                  <Container fluid>
-                    <Dropdown icon='plus'>
-                      <Dropdown.Menu direction='left' >
-                        <Dropdown.Item text='New Category' />
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Container>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          } 
-        />
-        </Menu> */}
+        /> */}
 
         <Grid.Column style={{marginLeft: 320}}>
           <React.Fragment></React.Fragment>
--- src/components/Chatroom/Chatroom.jsx
+++ src/components/Chatroom/Chatroom.jsx
@@ -15,55 +15,23 @@ class Chatroom extends React.Component {
     const {user} = this.props;
     return !user ? <Spinner /> : (
       <Grid columns='equal' className='app'>
-        <Sidebar 
+        {/* <Sidebar 
           width='very thin'
           icon='labeled'
           as={Menu}
           inverted
           vertical
           visible
-        />
+        /> */}
 
 
-         {/* <Menu
-        size='large' 
-        fixed='left'
-        vertical
-        // style={{paddingTop: '30px'}}
-        className='Chatroom_Header'
-      >
-        <Menu.Header 
-          as='div' 
-          content={
-            <Grid>
-              <Grid.Row columns='2'>
-                <Grid.Column verticalAlign='middle' width={12}>
-                  <Container style={{width: '100%', overflow: 'hidden'}} >
-                    {'Chatroom Name'}
-                  </Container>
-                </Grid.Column>
-                <Grid.Column verticalAlign='middle' width={2}>
-                  <Container fluid>
-                    <Dropdown icon='plus'>
-                      <Dropdown.Menu direction='left' >
-                        <Dropdown.Item text='New Category' />
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Container>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          } 
-        />
-        </Menu> */}
-
-        <Grid.Column style={{marginLeft: 320, padding: '0px'}} >
+        <Grid.Column style={{marginLeft: 320}} >
           <React.Fragment>
             {/* <ChatCommentPanel  channels={channels} user={user} chatroom={chatroom} getChannelComments={this.props.getChannelComments} /> */}
           </React.Fragment>
         </Grid.Column>
         <Grid.Column width={2}>
-          <React.Fragment></React.Fragment>
+          <React.Fragment> </React.Fragment>
         </Grid.Column>
       </Grid>
     );
