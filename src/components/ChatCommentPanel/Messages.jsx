22d3a7d02434463bfe2112ecf35e801d7acdab8c
Miguel <mgarcia95951@gmail.com>
2019-02-02 00:52:16 -0600

    Create messages component

---
+++ src/components/ChatCommentPanel/Messages.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Messages extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <h1>MEssage Form</h1>
+    )
+  }
+}
+
+export default Messages;
