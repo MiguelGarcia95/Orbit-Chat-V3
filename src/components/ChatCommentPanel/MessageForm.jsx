3d00e583fd19e316985a1f6eae6ad39bc0e6a4e2
Miguel <mgarcia95951@gmail.com>
2019-02-02 00:49:18 -0600

    create message form

---
+++ src/components/ChatCommentPanel/MessageForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MessageForm extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <h1>MEssage Form</h1>
+    )
+  }
+}
+
+export default MessageForm;
