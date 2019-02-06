9f479042e1f4fcfecea371de384903db5982cf2f
Miguel <mgarcia95951@gmail.com>
2019-02-05 20:35:04 -0600

    pass new comments to current comments

--- src/components/ChatCommentPanel/Message.jsx
+++ src/components/ChatCommentPanel/Message.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {Comment} from 'semantic-ui-react';
 import moment from 'moment';
 
-const timeFromNow = timestamp => moment(timestamp).fromNow();
+const timeFromNow = timestamp => {
+  
+  return moment(timestamp.createdAt.toDate()).fromNow();
+}
 
 const isOwnMessage = (message, user) => {
   return message.uid === user.uid ? 'message_self' : ''
@@ -14,7 +17,7 @@ const Message = ({message, user}) => {
       <Comment.Avatar src={message.avatar}  />
       <Comment.Content className={isOwnMessage(message, user)} >
         <Comment.Author as='a'>{message.username}</Comment.Author>
-        {message.createdAt && message.createdAt.toDate() && <Comment.Metadata >{timeFromNow(message.createdAt.toDate())}</Comment.Metadata> }
+        {message.createdAt &&  <Comment.Metadata >{timeFromNow(message)}</Comment.Metadata> }
         <Comment.Text>{message.comment}</Comment.Text>
       </Comment.Content>
     </Comment>
