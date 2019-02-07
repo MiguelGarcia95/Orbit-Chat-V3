316ce5432009e0aa126367fd96d660c9bf423159
Miguel <mgarcia95951@gmail.com>
2019-02-06 21:20:02 -0600

    added more functionality reducer and actions

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -11,6 +11,31 @@ const homeReducer = (state = initialState, action) => {
         ...state,
         homeroomError: action.payload.homeroomError
       }
+    case actionTypes.DELETE_DIRECT_MESSAGE:
+      return {
+        ...state,
+        homeroomError: action.payload.homeroomError
+      }
+    case actionTypes.JOIN_CHATROOM:
+      return {
+        ...state,
+        homeroomError: action.payload.homeroomError
+      }
+    case actionTypes.LEAVE_CHATROOM:
+      return {
+        ...state,
+        homeroomError: action.payload.homeroomError
+      }
+    case actionTypes.ADD_FRIEND:
+      return {
+        ...state,
+        homeroomError: action.payload.homeroomError
+      }
+    case actionTypes.REMOVE_FRIEND:
+      return {
+        ...state,
+        homeroomError: action.payload.homeroomError
+      }
     default:
       return state;
   }
