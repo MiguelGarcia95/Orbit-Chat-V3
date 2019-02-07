ca897ee0a09bb1c7763e0dbb9d5401d95daebb5c
Miguel <mgarcia95951@gmail.com>
2019-02-06 22:19:48 -0600

    added homeroom actions

--- src/actions/homeActions.js
+++ src/actions/homeActions.js
@@ -1,2 +1,41 @@
 import * as actionTypes from './types';
 
+export const createDirectMessage = message => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(message)
+  }
+}
+
+export const deleteDirectMessage = message => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(message.id)
+  }
+}
+
+export const joinChatroom = (user, chatroom) => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(user);
+    console.log(chatroom)
+  }
+};
+
+export const leaveChatroom = (user, chatroom) => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(user);
+    console.log(chatroom)
+  }
+};
+
+export const addFriend = (user, friend) => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(user);
+    console.log(friend)
+  }
+};
+
+export const removeFriend = (user, friend) => {
+  return (dispatch, getState, {getFirestore}) => {
+    console.log(user);
+    console.log(friend)
+  }
+};
