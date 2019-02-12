858a478a417c40ba662e857218a8a13ceea82113
Miguel <mgarcia95951@gmail.com>
2019-02-12 09:36:15 -0600

    get dms

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../actions/types';
 const initialState = {
   homeroomError: null,
   references: [],
-  currentView: 'friends',
+  currentView: '',
   directMessages: []
 }
 
