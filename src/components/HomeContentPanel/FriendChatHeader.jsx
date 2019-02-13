ac5c5b4186bdb62aae137da0416ae15efcb344b9
Miguel <mgarcia95951@gmail.com>
2019-02-12 21:42:46 -0600

    get dms

--- src/components/HomeContentPanel/FriendChatHeader.jsx
+++ src/components/HomeContentPanel/FriendChatHeader.jsx
@@ -4,7 +4,7 @@ import {Grid} from 'semantic-ui-react';
 const FriendChatHeader = () => {
   return (
     <Grid className='chat_comment_header'>
-      <Grid.Row columns='1' style={{paddingBottom: 0}} >
+      <Grid.Row style={{paddingBottom: 0}} >
         <Grid.Column verticalAlign='middle' >
           <p><strong>Avatar - </strong> Username</p>
         </Grid.Column>
