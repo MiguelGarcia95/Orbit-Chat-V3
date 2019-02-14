cbfc9e783444b2eed75fbb0103cd7238a2da936a
Miguel <mgarcia95951@gmail.com>
2019-02-14 10:16:58 -0600

    signout

--- src/components/RoomNavbar/UserPanel.jsx
+++ src/components/RoomNavbar/UserPanel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Image, Container, Modal, Segment, Button, Label, Input, Icon} from 'semantic-ui-react';
+import {Grid, Image, Container, Modal, Segment, Button, Label, Input, Icon, Dropdown} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {signOut} from '../../actions/authActions';
 
