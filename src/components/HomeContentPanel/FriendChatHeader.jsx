254d87ddbdca5c980cd326cbdd743b064e9ee684
Miguel <mgarcia95951@gmail.com>
2019-02-14 18:48:04 -0600

    delete dm chat

--- src/components/HomeContentPanel/FriendChatHeader.jsx
+++ src/components/HomeContentPanel/FriendChatHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Container, Header, Image} from 'semantic-ui-react';
+import {Grid, Container, Header, Image, Icon} from 'semantic-ui-react';
 
 const FriendChatHeader = ({friend}) => {
   return (
@@ -8,6 +8,8 @@ const FriendChatHeader = ({friend}) => {
         <Container fluid textAlign='right' >
           <Image circular src={friend.avatar} size='mini' floated='left' verticalAlign="middle" style={{marginBottom: '0', marginTop: '7.5px'}} />
           <Header as='h5' floated='left' style={{lineHeight: '50px'}} >{friend.username}</Header>
+          {/* 21 height of icon */}
+          <Icon name='trash alternate outline' size='large' color='red' style={{marginBottom: '0', marginTop: '14.5px', cursor: 'pointer'}} />
         </Container>
       </Grid.Row>
     </Grid>
