f1b9547aad470f09e63e5230847487a23015b06e
Miguel <mgarcia95951@gmail.com>
2019-02-14 19:39:05 -0600

    delete dm chat

--- src/components/HomeContentPanel/FriendChatHeader.jsx
+++ src/components/HomeContentPanel/FriendChatHeader.jsx
@@ -9,7 +9,12 @@ const FriendChatHeader = ({friend}) => {
           <Image circular src={friend.avatar} size='mini' floated='left' verticalAlign="middle" style={{marginBottom: '0', marginTop: '7.5px'}} />
           <Header as='h5' floated='left' style={{lineHeight: '50px'}} >{friend.username}</Header>
           {/* 21 height of icon */}
-          <Icon name='trash alternate outline' size='large' color='red' style={{marginBottom: '0', marginTop: '14.5px', cursor: 'pointer'}} />
+          <Icon 
+            name='trash alternate outline' 
+            size='large' color='red' 
+            
+            style={{marginBottom: '0', marginTop: '14.5px', cursor: 'pointer'}}
+          />
         </Container>
       </Grid.Row>
     </Grid>
