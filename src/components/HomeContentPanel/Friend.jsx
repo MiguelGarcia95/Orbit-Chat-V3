4463b4253c2f7c520891bda71277853437846b38
Miguel <mgarcia95951@gmail.com>
2019-02-19 20:22:42 -0600

    display friends

--- src/components/HomeContentPanel/Friend.jsx
+++ src/components/HomeContentPanel/Friend.jsx
@@ -6,9 +6,9 @@ const Friend = ({friend}) => {
   return (
     <List.Item className='chat_comment'>
       <List.Content floated='right'>
-        <Button color='red'>Delete</Button>
-        <Button color='green'>Accept</Button>
-        <Button color='orange'>Reject</Button>
+        <Button color='red' content='Delete' icon='trash alternate outline' labelPosition='right' />
+        <Button content='Accept' color='green' icon='check' labelPosition='right' />
+        <Button content='Reject' color='orange' icon='user cancel' labelPosition='right' />
       </List.Content>
       <Image avatar src={friend.avatar} />
       <List.Content className={`comment_body`}>{friend.username}</List.Content>
