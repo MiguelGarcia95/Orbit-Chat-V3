ab49c9b8d90017b0b2917b6eb00d666df2d4f86a
Miguel <mgarcia95951@gmail.com>
2019-02-19 18:10:47 -0600

    display friends

--- src/components/HomeContentPanel/FriendListHeader.jsx
+++ src/components/HomeContentPanel/FriendListHeader.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {Grid, Container, Header, Image} from 'semantic-ui-react';
+import {Grid, Container, Header} from 'semantic-ui-react';
 
-const FriendListHeader = () => {
-  return (
-    <Grid className='home_comment_header'>
-      <Grid.Row columns='1' style={{padding: '0px'}}>
-        <Container fluid textAlign='right' >
-          <Header as='h4' floated='left' style={{lineHeight: '50px'}} >All</Header>
-          <Header as='h4' floated='left' style={{lineHeight: '50px'}} >Friends</Header>
-          <Header as='h4' floated='left' style={{lineHeight: '50px'}} >Pending</Header>
-        </Container>
-      </Grid.Row>
-    </Grid>
-  )
+class FriendListHeader extends React.Component {
+  render() {
+    return (
+      <Grid className='home_comment_header'>
+        <Grid.Row columns='1' style={{padding: '0px'}}>
+          <Container fluid textAlign='right' >
+            <Header as='h4' floated='left' style={{lineHeight: '50px'}} >All</Header>
+            <Header as='h4' floated='left' style={{lineHeight: '50px'}} >Friends</Header>
+            <Header as='h4' floated='left' style={{lineHeight: '50px'}} >Pending</Header>
+          </Container>
+        </Grid.Row>
+      </Grid>
+    )
+  }
 }
 
 export default FriendListHeader;
