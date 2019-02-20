ca541e45628d4c6d080161eaf0336f3c9a7ef3e3
Miguel <mgarcia95951@gmail.com>
2019-02-19 21:26:38 -0600

    display friend buttons

--- src/components/HomeContentPanel/Friend.jsx
+++ src/components/HomeContentPanel/Friend.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import {List, Button, Image} from 'semantic-ui-react';
 
-const getFriendStatus = friend => {
+const displayButtons = friend => {
   if (friend.status === 'accepted') {
-
+    return (
+      <React.Fragment>
+        <Button color='red' content='Delete' icon='trash alternate outline' labelPosition='right' />
+      </React.Fragment>
+    )
   } else if (friend.status === 'rejected') {
-
+    return (
+      <React.Fragment>
+        <Button color='red' content='Delete' icon='trash alternate outline' labelPosition='right' />
+      </React.Fragment>
+    )
   } else {
-    
+    return (
+      <React.Fragment>
+        <Button content='Accept' color='green' icon='check' labelPosition='right' />
+        <Button content='Reject' color='orange' icon='user cancel' labelPosition='right' />
+        <Button content='Waiting' color='grey' icon='user cancel' labelPosition='right' disabled />
+      </React.Fragment>
+    )
   }
 }
 
@@ -16,9 +30,7 @@ const Friend = ({friend}) => {
   return (
     <List.Item className='chat_comment'>
       <List.Content floated='right'>
-        <Button color='red' content='Delete' icon='trash alternate outline' labelPosition='right' />
-        <Button content='Accept' color='green' icon='check' labelPosition='right' />
-        <Button content='Reject' color='orange' icon='user cancel' labelPosition='right' />
+        {displayButtons(friend)}
       </List.Content>
       <Image avatar src={friend.avatar} />
       <List.Content className={`comment_body`}>{friend.username}</List.Content>
