862c86575221ebbb7d476c3a8b1a52c65b8de3fa
Miguel <mgarcia95951@gmail.com>
2019-02-22 23:48:10 -0600

    accept or reject chatroom invitation

--- src/components/HomeContentPanel/ChatroomInvite.jsx
+++ src/components/HomeContentPanel/ChatroomInvite.jsx
@@ -3,45 +3,40 @@ import {connect} from 'react-redux';
 import {List, Button, Image, Icon} from 'semantic-ui-react';
 import {joinChatroom, rejectChatroomInvitation} from '../../actions/chatroomActions';
 
-class ChatroomInvite extends React.Component {
-
-  render() {
-    const {chatroomInvite, user} = this.props
-    console.log(chatroomInvite)
-    return (
-      <List.Item className='chat_comment'>
-        <List.Content floated='right'>
-          <Button circular
-            animated='fade' color='green'
-            style={{padding: '10px 20px'}} 
-            onClick={() => this.props.joinChatroom(user, chatroomInvite)}
-          >
-            <Button.Content hidden>Join</Button.Content>
-            <Button.Content visible>
-              <Icon name='check' />
-            </Button.Content>
-          </Button>
-          <Button circular
-            animated='fade' color='orange'
-            style={{padding: '10px 20px'}} 
-            onClick={() => this.props.rejectChatroomInvitation(user.id, chatroomInvite.id)}
-          >
-            <Button.Content hidden>Reject</Button.Content>
-            <Button.Content visible>
-              <Icon name='ban' />
-            </Button.Content>
-          </Button>
-        </List.Content>
-        <Image avatar src={chatroomInvite.chatroom.avatar} />
-        <List.Content 
-          className={`comment_body`} 
-          as='p'
+const ChatroomInvite = ({chatroomInvite, user}) => {
+  return (
+    <List.Item className='chat_comment'>
+      <List.Content floated='right'>
+        <Button circular
+          animated='fade' color='green'
+          style={{padding: '10px 20px'}} 
+          onClick={() => this.props.joinChatroom(user, chatroomInvite)}
         >
-          {chatroomInvite.chatroom.name}
-        </List.Content>
-      </List.Item>
-    )
-  }
+          <Button.Content hidden>Join</Button.Content>
+          <Button.Content visible>
+            <Icon name='check' />
+          </Button.Content>
+        </Button>
+        <Button circular
+          animated='fade' color='orange'
+          style={{padding: '10px 20px'}} 
+          onClick={() => this.props.rejectChatroomInvitation(user.id, chatroomInvite.id)}
+        >
+          <Button.Content hidden>Reject</Button.Content>
+          <Button.Content visible>
+            <Icon name='ban' />
+          </Button.Content>
+        </Button>
+      </List.Content>
+      <Image avatar src={chatroomInvite.chatroom.avatar} />
+      <List.Content 
+        className={`comment_body`} 
+        as='p'
+      >
+        {chatroomInvite.chatroom.name}
+      </List.Content>
+    </List.Item>
+  )
 }
 
 const mapDispatchToProps = dispatch => {
