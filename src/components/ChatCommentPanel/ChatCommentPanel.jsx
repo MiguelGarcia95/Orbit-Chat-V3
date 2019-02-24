ab5901c7427bb55323460d781aacbcd089f8f79f
Miguel <mgarcia95951@gmail.com>
2019-02-23 23:24:38 -0600

    only send friendrequest to non friends

--- src/components/ChatCommentPanel/ChatCommentPanel.jsx
+++ src/components/ChatCommentPanel/ChatCommentPanel.jsx
@@ -6,11 +6,11 @@ import Messages from './Messages';
 import MessageForm from './MessageForm';
 
 const ChatCommentPanel = (props) => {
-    const {channel, user, comments, chatroomUsers, chatroomInvites} = props;
+    const {channel, user, comments, friendsList} = props;
     return (
       <section className='chat_comment_panel'>
         <ChatCommentHeader channel={channel} />
-        {comments && <Messages messages={comments} user={user} chatroomUsers={chatroomUsers} chatroomInvites={chatroomInvites} />}
+        {comments && <Messages messages={comments} user={user} friendsList={friendsList} />}
         <MessageForm channel={channel} user={user}  />
       </section>
     )
@@ -19,8 +19,7 @@ const ChatCommentPanel = (props) => {
 const mapStateToProps = state => {
   return {
     comments: state.channel.comments,
-    chatroomUsers: state.chat.chatroomUsers,
-    chatroomInvites: state.chat.chatroomInvites
+    friendsList: state.home.friendsList
   }
 }
 
--- src/components/ChatCommentPanel/Messages.jsx
+++ src/components/ChatCommentPanel/Messages.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import Message from './Message';
 import {Comment} from 'semantic-ui-react';
 
-const displayMessages = (messages, user) => {
+const displayMessages = (messages, user, friendsList) => {
   return messages.map(message => {
     return (
       <Message 
         user={user} 
         key={message.id} 
         messageId={message.id} 
-        message={message.message} 
+        message={message.message}
+        friendsList={friendsList}
       />
     )
   })
 }
 
-const Messages = ({messages, user}) => {
+const Messages = ({messages, user, friendsList}) => {
   return (
     <Comment.Group className='chat_comment_container'>
-      {displayMessages(messages, user)}
+      {displayMessages(messages, user, friendsList)}
     </Comment.Group>
   )
 }
