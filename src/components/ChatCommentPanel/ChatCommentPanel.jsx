4efb2e773b6188d4a99aa23f5d43260614a3e62c
Miguel <mgarcia95951@gmail.com>
2019-02-23 23:12:45 -0600

    only let creator add channels to chat

--- src/components/ChatCommentPanel/ChatCommentPanel.jsx
+++ src/components/ChatCommentPanel/ChatCommentPanel.jsx
@@ -6,11 +6,11 @@ import Messages from './Messages';
 import MessageForm from './MessageForm';
 
 const ChatCommentPanel = (props) => {
-    const {channel, user, comments} = props;
+    const {channel, user, comments, chatroomUsers, chatroomInvites} = props;
     return (
       <section className='chat_comment_panel'>
         <ChatCommentHeader channel={channel} />
-        {comments && <Messages messages={comments} user={user} />}
+        {comments && <Messages messages={comments} user={user} chatroomUsers={chatroomUsers} chatroomInvites={chatroomInvites}  />}
         <MessageForm channel={channel} user={user}  />
       </section>
     )
@@ -18,7 +18,9 @@ const ChatCommentPanel = (props) => {
 
 const mapStateToProps = state => {
   return {
-    comments: state.channel.comments
+    comments: state.channel.comments,
+    chatroomUsers: state.chat.chatroomUsers,
+    chatroomInvites: state.chat.chatroomInvites
   }
 }
 
