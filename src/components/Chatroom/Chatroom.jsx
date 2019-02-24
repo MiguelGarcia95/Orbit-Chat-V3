ac54f0db764391f2681c31c1a8c3061dfa94834b
Miguel <mgarcia95951@gmail.com>
2019-02-24 16:37:59 -0600

    redirect of not a member

--- src/components/Chatroom/Chatroom.jsx
+++ src/components/Chatroom/Chatroom.jsx
@@ -47,7 +47,7 @@ class Chatroom extends React.Component {
       }
       this.props.getChatroomUsers(nextProps.currentChatroom.id);
     } else if (nextProps.chatroomUsers.length > 0) {
-      console.log(this.isUserAMember(nextProps.user, nextProps.chatroomUsers))
+      // console.log(this.isUserAMember(nextProps.user, nextProps.chatroomUsers))
     }
 
     this.setCurrentChannel(nextProps.currentChannel, nextProps.channels, nextProps.categories, true);
