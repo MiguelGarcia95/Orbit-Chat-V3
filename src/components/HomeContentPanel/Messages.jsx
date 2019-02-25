114aa4a38e6fc0f03943e53adeedf26a58b84584
Miguel <mgarcia95951@gmail.com>
2019-02-24 22:31:20 -0600

    scroll chat to bottom

--- src/components/HomeContentPanel/Messages.jsx
+++ src/components/HomeContentPanel/Messages.jsx
@@ -2,34 +2,38 @@ import React from 'react';
 import Message from './Message';
 import {Comment} from 'semantic-ui-react';
 
-const scrollToBottom = () => {
-  let commentBox = document.getElementById('chat_box');
-  // commentBox.scrollTop = commentBox.scrollHeight;
-  console.log(commentBox.clientHeight);
-}
+class Messages extends React.Component {
 
-const displayMessages = (messages, user, otherUser, friends) => {
-  return messages.map(message => {
+  scrollToBottom = () => {
+    let commentBox = document.getElementById('chat_box');
+    // commentBox.scrollTop = commentBox.scrollHeight;
+    console.log(commentBox.clientHeight);
+  }
+
+  displayMessages = (messages, user, otherUser, friends) => {
+    return messages.map(message => {
+      return (
+        <Message 
+          user={user} 
+          key={message.id} 
+          otherUser={otherUser} 
+          messageId={message.id} 
+          message={message.message} 
+          friends={friends}
+        />
+      )
+    })
+  }
+
+  render() {
+    const {messages, user, otherUser, friends} = this.props;
+    this.scrollToBottom()
     return (
-      <Message 
-        user={user} 
-        key={message.id} 
-        otherUser={otherUser} 
-        messageId={message.id} 
-        message={message.message} 
-        friends={friends}
-      />
+      <Comment.Group className='chat_comment_container' id='chat_box'>
+        {this.displayMessages(messages, user, otherUser, friends)}
+      </Comment.Group>
     )
-  })
-}
-
-const Messages = ({messages, user, otherUser, friends}) => {
-  scrollToBottom()
-  return (
-    <Comment.Group className='chat_comment_container' id='chat_box'>
-      {displayMessages(messages, user, otherUser, friends)}
-    </Comment.Group>
-  )
+  }
 }
 
 export default Messages;
