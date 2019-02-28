17532f79bdb402680085ec71574bd6d38572c6ed
Miguel <mgarcia95951@gmail.com>
2019-02-28 16:43:56 -0600

    change friend display on click

--- src/components/HomeContentPanel/FriendListHeader.jsx
+++ src/components/HomeContentPanel/FriendListHeader.jsx
@@ -6,9 +6,9 @@ const FriendListHeader = ({setFriendDisplay, friendsToShow}) => {
     <Grid className='home_comment_header'>
       <Grid.Row columns='1' style={{padding: '0px'}}>
         <Container fluid textAlign='right' >
-          <Header className='home_header_option active' as='h4' floated='left' style={{lineHeight: '50px', cursor: 'pointer'}} onClick={() => setFriendDisplay('all')} >All</Header>
-          <Header className='home_header_option' as='h4' floated='left' style={{lineHeight: '50px', cursor: 'pointer'}} onClick={() => setFriendDisplay('friends')} >Friends</Header>
-          <Header as='h4' floated='left' style={{lineHeight: '50px', cursor: 'pointer'}} onClick={() => setFriendDisplay('pending')} >Pending</Header>
+          <Header className='home_header_option active' as='h4' floated='left' onClick={() => setFriendDisplay('all')} >All</Header>
+          <Header className='home_header_option' as='h4' floated='left' onClick={() => setFriendDisplay('friends')} >Friends</Header>
+          <Header className='home_header_option' as='h4' floated='left' onClick={() => setFriendDisplay('pending')} >Pending</Header>
         </Container>
       </Grid.Row>
     </Grid>
