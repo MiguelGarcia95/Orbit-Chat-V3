3891ce358733704b76fdd5732e1cbcb7aa3fad53
Miguel <mgarcia95951@gmail.com>
2019-02-28 18:33:13 -0600

    change friend display on click

--- src/components/HomeContentPanel/FriendList.jsx
+++ src/components/HomeContentPanel/FriendList.jsx
@@ -8,10 +8,33 @@ const displayFriends = (friends, user) => {
   })
 }
 
-const FriendList = ({friends, user}) => {
+const filterFriends = (friends, friendsToShow) => {
+  if (friendsToShow === 'all') {
+    return friends
+  } else if (friendsToShow === 'friends') {
+    let allFriends = [];
+    friends.forEach(friend => {
+      if (friend.friend.status === 'accepted') {
+        allFriends.push(friend);
+      }
+    });
+    return allFriends;
+  } else {
+    let allFriends = [];
+    friends.forEach(friend => {
+      if (friend.friend.status === 'pending') {
+        allFriends.push(friend);
+      }
+    });
+    return allFriends;
+  }
+}
+
+const FriendList = ({friends, user, friendsToShow}) => {
+  const sortedFriends = filterFriends(friends, friendsToShow);
   return (
     <List divided verticalAlign='middle' className='chat_comment_container'>
-      {displayFriends(friends, user)}
+      {displayFriends(sortedFriends, user)}
     </List>
   )
 }
