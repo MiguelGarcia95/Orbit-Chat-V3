8cc7a230a035e9aabd8cd17ef5ecbc21b42a67d0
Miguel <mgarcia95951@gmail.com>
2019-03-02 15:07:06 -0600

    Disabled devtools

--- src/store.js
+++ src/store.js
@@ -18,7 +18,7 @@ const store = createStore(
     applyMiddleware(...middleware),
     reduxFirestore(firebase),
     reactReduxFirebase(firebase),
-    composeWithDevTools()
+    // composeWithDevTools()
   )
 )
 
